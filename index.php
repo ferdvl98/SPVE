f55577685c7ed4ed392f566d93b873cfe199b5ed
Fernando Dávila Aguilar <ferdvl98@outlook.com>
2021-03-21 10:50:29 -0600

    css

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login - Registro</title>
+    <link rel = "stylesheet" href="assets/css/estilos.css">
 </head>
 <body>
+    <div class="contenedor__todo">
+        <div class="caja__trasera">
+            <div class="caja__trasera_login">
+                <h3>¿Ya tienes una cuenta?</h3>
+                <p>Inicia sesión</p>
+                <button id="btn__iniciar-sesion">Iniciar Sesión</button>
+            </div>
+
+            <div class="caja__trasera_register">
+                <h3>¿Ya tienes una cuenta?</h3>
+                <p>Inicia sesión</p>
+                <button id="btn__iniciar-sesion">Iniciar Sesión</button>
+            </div>
+        </div>    
+    </div>
 </body>
 </html>
