47349c61b0c09d915c807b07eabb0f55e2728a59
ferdvl98 <ferdvl98@gmail.com>
2021-03-31 16:33:32 -0600

    código de Barras

--- CB/codigo.php
+++ CB/codigo.php
@@ -1,14 +1,58 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <script src="../assets/js/bootstrap.min.js"></script>
-    <link rel = "stylesheet" href="../assets/css/bootstrap.min.css">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <div class="container"><br><br> 
+        <legend>Productos</legend>
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4">Producto</label>
+                    <input type="text" class="form-control" id="inputProduct" placeholder="Nombre del producto">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Proveedor</label>
+                    <input type="text" class="form-control" id="inputSupplier" placeholder="Nombre del proveedor">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4">Precio</label>
+                    <input type="number" class="form-control" id="inputPrice" placeholder="Precio del producto">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Stock</label>
+                    <input type="number" class="form-control" id="inputStock" placeholder="Cantidad disponible">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputCity">Descipción</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">Categoria</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputZip">Código de Barras</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+            <button type="submit" class="btn btn-primary">Crear Código</button>
+        </form>
+    </div>
 </body>
+
 </html>
